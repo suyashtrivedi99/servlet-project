0a020f6ab7768781a24946cdcf580f2f632462a9
suyashtrivedi99 <suyash.trivedi99@gmail.com>
2021-10-10 21:27:16 +0530

    POST Requests handled in SimpleServlet

--- src/main/java/com/example/SimpleServlet.java
+++ src/main/java/com/example/SimpleServlet.java
@@ -10,8 +10,25 @@ import java.io.IOException;
 @WebServlet("/home")
 public class SimpleServlet extends HttpServlet {
 
+    //All get requests to the servlet are handled here
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello World");
+        String name = req.getParameter("name");
+        if(name != null) {
+            resp.getWriter().printf("Hello %s!", name);
+        } else {
+            resp.getWriter().write("Please enter a name!");
+        }
+    }
+
+    //All post requests to the servlet are handled here
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        if(name != null && name != "") {
+            resp.getWriter().printf("Hello %s!", name);
+        } else {
+            resp.getWriter().write("Please enter a name!");
+        }
     }
 }
